e03e1e2c01799ffc340fb54493c0373582851186
Anrew Krivenko <demize26rus@gmail.com>
2022-12-11 21:25:33 +0300

    init commit

---
+++ W1/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+Console.WriteLine("Enter the count of numbers");
+int count = int.Parse(Console.ReadLine());
+int[] userArray = new int[count];
+FillUserArray(userArray);
+int countNegative = GetNegetiveNumbersOfArray(userArray);
+Console.WriteLine($"Count negative numbers = {countNegative}");
+
+
+void FillUserArray(int[] array)
+{
+	Console.WriteLine($"Enter {array.Length} numbers");
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = int.Parse(Console.ReadLine());
+	}
+}
+
+int GetNegetiveNumbersOfArray(int[] array)
+{
+	int count = 0;
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (array[i] < 0)
+		{
+			count++;
+		}
+	}
+	return count;
+}
---
+++ W2/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+using static Common.Helper;
+double b1 = UserInputDouble("Enter b1");
+double k1 = UserInputDouble("Enter k1");
+double b2 = UserInputDouble("Enter b2");
+double k2 = UserInputDouble("Enter k2");
+
+if (k1 == k2)
+{
+	Console.WriteLine("it is impossible to find the coordinates of the point of intersection of the given lines, since these lines are parallel.");
+}
+
+GetPointCoordinates(k1, k2, b1, b2, out double x, out double y);
+Console.WriteLine($"({x};{y})");
+
+void GetPointCoordinates(double k1, double k2, double b1, double b2, out double x, out double y)
+{
+	double x = (b1 - b2) / (k2 - k1);
+	double y = (k2 * x + b2);
+}
